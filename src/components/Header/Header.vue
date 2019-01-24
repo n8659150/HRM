5ae511e81b0a3b6b0369fd3c6ff47d554ef59165
JackLi <jack.li@careerbuilder.com>
2019-01-24 15:33:28 +0800

    remove Vant

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,12 +1,5 @@
 <template>
-  <van-nav-bar
-    title="标题"
-    left-text="返回"
-    right-text="按钮"
-    left-arrow
-    @click-left="onClickLeft"
-    @click-right="onClickRight"
-  />
+  <header>Header</header>
 </template>
 
 <script>
@@ -18,12 +11,6 @@ export default {
     };
   },
    methods: {
-    onClickLeft() {
-      Toast('返回');
-    },
-    onClickRight() {
-      Toast('按钮');
-    }
   }
 };
 </script>
--- src/components/ResumeList/ResumeList.vue
+++ src/components/ResumeList/ResumeList.vue
@@ -1,7 +1,9 @@
 <template>
-  <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
-    <van-cell v-for="item in list" :key="item" :title="item"/>
-  </van-list>
+<ul>
+  <li v-for="item in list" :key="item">
+    {{item}}
+  </li>
+</ul>
 </template>
 
 <script>
@@ -9,28 +11,12 @@ export default {
   name: "ResumeList",
   data() {
     return {
-      list: [],
-      loading: false,
-      finished: false
+      list: ['1','2','3','4','5']
     };
   },
 
   methods: {
-    onLoad() {
-      // 异步更新数据
-      setTimeout(() => {
-        for (let i = 0; i < 10; i++) {
-          this.list.push(this.list.length + 1);
-        }
-        // 加载状态结束
-        this.loading = false;
 
-        // 数据全部加载完成
-        if (this.list.length >= 40) {
-          this.finished = true;
-        }
-      }, 500);
-    }
   }
 };
 </script>
