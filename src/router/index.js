3debe9261a7e8a45be3fe8c4932bd34fdcbb1dec
shenghua-cb <shenghua.lian@careerbuilder.com>
2019-01-24 16:40:56 +0800

    add bootstrap

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
 import Router from 'vue-router'
 import ResumeList from '@/components/ResumeList/ResumeList.vue'
 import ResumeDetail from '@/components/ResumeDetail/ResumeDetail.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+Vue.use(BootstrapVue);
 Vue.use(Router)
 
 export default new Router({
@@ -12,8 +16,8 @@ export default new Router({
       name: 'ResumeList',
       component: ResumeList
     },
-    { 
-      path: '/resume/:id', 
+    {
+      path: '/resume/:id',
       name: 'ResumeDetail',
       component: ResumeDetail
     }
